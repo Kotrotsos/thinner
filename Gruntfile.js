e0e1b57acb8b932073f7f6ef8b15967ce4e799d2
Marco Kotrotsos <kotrotsos@gmail.com>
2017-04-10 16:49:43 +0200

    added grunt tasks, babel and sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,46 @@
 module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-babel');
 
     grunt.initConfig({
         sass: {
             options: {
-                style: 'expanded'
+                style: 'compact',
+                lineNumbers: true
             },
             dist: {
                 files: {
                     'build/main.css': 'src/sass/base.scss'
                 }
             }
+        },
+        watch: {
+            all: {
+                files: ['src/**/*.*'],
+                tasks: ['sass', 'babel'],
+                options: {
+                    spawn: true,
+                    livereload: {
+                        host: 'localhost',
+                        port: 9000
+                    }
+                },
+            },
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+                presets: ['es2015']
+            },
+            dist: {
+                files: {
+                    'build/main.js': 'src/js/*.js'
+                }
+            }
         }
     });
 
-    grunt.registerTask('default', ['sass']);
+    grunt.registerTask('default', ['sass', 'babel']);
 }
